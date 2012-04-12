c24b744d9878cdf789218c480ded45fe694525f3
SSingh <ss@ss44.ca>
2012-04-12 00:59:24 -0400

    - set additional parameters in the sag output including xCenter, yCenter, and rotate to save on potential calculations later.

--- svgToLua.php
+++ svgToLua.php
@@ -14,18 +14,23 @@ if (!array_key_exists(1, $argv)){
 }
 
 $svgFileName = $argv[1];
-
 svgToLua( $svgFileName );
 
+
+
+/**
+ * core function to parse the svg file.
+ */
 function svgToLua( $filename ){
 
 	// Get the contents of the svg file.
 	$content = file_get_contents( $filename );
+	$userMethods = userParseMethods();
 
 	$svg = new SimpleXMLElement( $content );
 	$svg->svg;
 	$groups = array();
-
+	echo "module(...)\n";
 	echo "svg = {\n";
 
 	$notLast = array(false, false, false);
@@ -49,6 +54,8 @@ function svgToLua( $filename ){
 			echo "shape = '$type', ";
 
 			$notLast[2] = false;
+
+			// Save all the standard attributes
 			foreach ( $childNode->attributes() as $key => $value ){
 				if ( $notLast[2] ){
 					echo ', ';
@@ -56,7 +63,13 @@ function svgToLua( $filename ){
 
 				$key = str_replace('-', '_', $key);
 				echo "$key = '$value' "; 
+
 				$notLast[2] = true;
+			}
+
+			// Run some of our custom modules against this element and add these to the object also.
+			foreach( $userMethods as $method ){
+				call_user_func( $method, $childNode );
 			}
 
 			echo "}";
@@ -70,6 +83,49 @@ function svgToLua( $filename ){
 	
 
 	echo "\n}\n";
+}
+
+function _svgCalculateRotation( $node ){
+
+	if ( isset($node['transform'] ) ){
+		$transformation = $node['transform'];
+
+		// Deal with the matrix transforatmion
+		if ( !preg_match( '/matrix\((.*)\)/', $transformation, $tmp ) ){
+			return;
+		}
+
+		// Explode the coords
+		$coords = explode(' ', $tmp[1]);
+		$angle = atan2($coords[2], $coords[0]);
+		
+		// Convert from radians to degrees
+		$angle = round($angle * 180 / pi(), 2);
+
+		echo ", rotate = '$angle'";	
+	}	
+}
+
+// Calculate the center x, y coords
+function _svgCalculateCenters( $node ){
+
+	if (isset($node['x'], $node['y'], $node['height'], $node['width'] )){
+		$centerX = round( $node['width'] / 2 + $node['x'], 2);
+		$centerY = round( $node['height'] / 2 + $node['y'], 2);
+
+		echo ", xCenter = '$centerX', yCenter = '$centerY' ";
+	}
+}
+
+function userParseMethods(){
+	$methods = get_defined_functions();
+	$definedMethods = array();
+	foreach ( $methods['user'] as $method ){
+		if (strpos($method, '_svg') !== false){
+			$definedMethods[] = $method;
+		}
+	}
+	return $definedMethods;
 }
 
 function showUsage(){
